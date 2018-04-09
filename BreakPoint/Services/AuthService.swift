12fe2aead1df7062c2b755d9b36267fcf21cbf6e
JoeyTribbiani <vophanhongdung@gmail.com>
2018-04-09 15:05:27 +0700

    commit create file AutheService

---
+++ BreakPoint/Services/AuthService.swift
@@ -0,0 +1,41 @@
+//
+//  AuthService.swift
+//  BreakPoint
+//
+//  Created by Joey Tribbiani on 4/9/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthSerivce {
+    static let instance = AuthSerivce()
+    
+    func registerUser(withEmail email : String , andPassword password : String , completion : @escaping CompletionHandler ){
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                completion(false , error)
+                return
+            }
+            
+            let userData = [
+                "provider":user.providerID,
+                "email" :user.email
+            ]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            completion(true,nil)
+        }
+    }
+    
+    func loginUser(withEmail email : String , andPassword password : String , completion : @escaping CompletionHandler) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                completion(false ,error)
+                
+                return
+            }
+            completion(true,error)
+        }
+    }
+}
---
+++ BreakPoint/UItilites/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  BreakPoint
+//
+//  Created by Joey Tribbiani on 4/9/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import Foundation
+
+typealias CompletionHandler =  (_ success : Bool ,_ error : Error?) -> ()
